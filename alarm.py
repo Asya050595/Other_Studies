b1eded49b429f415ca41e7d29d6770c5e01b1129
Asya050595 <131971857+Asya050595@users.noreply.github.com>
2024-07-31 14:35:14 +0400

    Add files via upload

---
+++ alarm.py
@@ -0,0 +1,10 @@
+print('Hello! Please enter the current time (HH:MM): ')
+print('Please enter the duration of your sleep (HH:MM): ')
+
+current_time = int.input()
+duration = int.input()
+
+
+
+
+print('Your alarm has been set for {}.'.format(   ))
